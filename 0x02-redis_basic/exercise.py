bb81ddd699e570842ee49f9cbc0339099899fba8
Evodiatembi <evodytembi@gmail.com>
2024-07-18 20:02:01 +0000

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -26,3 +26,51 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self,
+            key: str,
+            fn: Optional[Callable[[bytes], Union[str,
+                                                 bytes, int, float]]] = None
+            ) -> Union[str, bytes, int, float]:
+        """
+        Retrieve the data from the cache using the provided key.
+
+        Args:
+            key (str): The key to retrieve the data.
+            fn (Optional[Callable[[bytes], Union[str, bytes, int, float]]]):
+            A function to apply to the retrieved data.
+
+        Returns:
+            Union[str, bytes, int, float, None]: The retrieved data,
+            possibly transformed by fn, or None if the key does not exist.
+        """
+        data = self._redis.get(key)
+        if data is not None and fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Retrieve the data as a string from the cache using the provided key.
+
+        Args:
+            key (str): The key to retrieve the data.
+
+        Returns:
+            Optional[str]: The retrieved data as a string,
+            or None if the key does not exist.
+        """
+        return self.get(key, fn=lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        Retrieve the data as an integer from the cache using the provided key.
+
+        Args:
+            key (str): The key to retrieve the data.
+
+        Returns:
+            Optional[int]: The retrieved data as an integer,
+            or None if the key does not exist.
+        """
+        return self.get(key, fn=int)
