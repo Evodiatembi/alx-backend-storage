8aac4e9a7487bc8017f0caa026ac404af54efb01
Evodiatembi <evodytembi@gmail.com>
2024-07-18 20:16:22 +0000

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,82 @@
 """
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Callable, Optional
+from functools import wraps
+
+def count_calls(method: Callable) -> Callable:
+    """
+     A decorator that counts the number of calls to the decorated function
+     Args:
+        method (Callable): The method to be decorated.
+     Returns:
+        Callable: The decorated method with counting functionality.
+    """
+    # wraps preserves the original method metadata
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """increment the call count using method's qualified name"""
+        # Get qualified name of the method
+        qualname = method.__qualname__
+
+        # Increment the call count in redis
+        self._redis.incr(qualname)
+
+        # Execute the original method and return its value
+        return method(self, *args, **kwargs)
+
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """
+    A decorator to store history of inputs and outputs for a function.
+    Args:
+         method (Callable): The method to be decorated.
+    Returns:
+         Callable: The decorated method with history functionality
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        qualname = method.__qualname__
+        inputs_key = "{}:inputs".format(qualname)
+        outputs_key = "{}:outputs".format(qualname)
+
+        # store the input arguments in redis
+        self._redis.rpush(inputs_key, str(args))
+        # call the actual method and get the result
+        res = method(self, *args, **kwargs)
+        # store the output result in redis
+        self._redis.rpush(outputs_key, str(res))
+
+        return res
+
+    return wrapper
+
+
+def replay(method: Callable) -> None:
+    """
+    Display the history of calls of a particular function
+    Args:
+        method (Callable): Method whose history is to be displayed
+    """
+    self = method.__self__
+    qualname = method.__qualname__
+    # Generate the keys for input and output history
+    inputs_key = "{}:inputs".format(qualname)
+    outputs_key = "{}:outputs".format(qualname)
+
+    # Retrieve the input and output history from redis
+    inputs = self._redis.lrange(inputs_key, 0, -1)
+    outputs = self._redis.lrange(outputs_key, 0, -1)
+
+    # Print the history
+    print("{} was called {} times:".format(qualname, len(inputs)))
+    for input_, output in zip(inputs, outputs):
+        print("{}(*{}) -> {}".format(
+                                     qualname,
+                                     input_.decode('utf-8'),
+                                     output.decode('utf-8')))
 
 class Cache:
     """ Cache class
